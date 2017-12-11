940e63f27b6a3c2eef379ebe74082ceb828b6651
leixiangjian <leixiangjian@bolemy.com>
2017-12-11 12:44:00 +0800

    commit;

---
+++ netty/src/main/java/com/lxj/netty/client/TimeClient.java
@@ -0,0 +1,43 @@
+package com.lxj.netty.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public class TimeClient {
+	public static void main(String[] args) {
+
+	}
+	
+	public void connect(int port,String host) throws Exception{
+		//定义io读写线程组
+		EventLoopGroup group = new NioEventLoopGroup();
+		try{
+			Bootstrap b = new Bootstrap();
+			b.group(group).channel(NioSocketChannel.class)
+			.option(ChannelOption.TCP_NODELAY, true)
+			.handler(new ChannelInitializer<SocketChannel>() {
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new TimeClientHandler());
+				}
+				
+			});
+			
+			ChannelFuture f = b.connect(host,port).sync();
+			f.channel().closeFuture().sync();
+		}finally{
+			group.shutdownGracefully();
+		}
+	}
+	
+	private class TimeClientHandler extends ChannelHandlerAdapter{
+		
+	}
+}
---
+++ netty/src/main/java/com/lxj/netty/server/TimeServer.java
@@ -0,0 +1,65 @@
+package com.lxj.netty.server;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+public class TimeServer {
+	public static void main(String[] args) throws Exception {
+		TimeServer timeServer = new TimeServer();
+		timeServer.bind(9001);
+	}
+	
+	private void bind(int port) throws Exception{
+		//两个线程组用户网络事件处理
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		
+		try{
+			ServerBootstrap serverBootstrap = new ServerBootstrap();
+			serverBootstrap.group(bossGroup, workerGroup)
+			.channel(NioServerSocketChannel.class)
+			.option(ChannelOption.SO_BACKLOG, 1024)
+			.childHandler(new ChannelInitializer<SocketChannel>(){
+				@Override
+				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new TimeServerHandler());					
+				}
+			});
+			
+			//绑定端口，同步等待
+			ChannelFuture f = serverBootstrap.bind(port).sync();
+			
+			//登陆服务器监听端口关闭
+			f.channel().closeFuture().sync();
+			
+			
+		}finally{
+			//优雅退出，释放资源
+			bossGroup.shutdownGracefully();
+			workerGroup.shutdownGracefully();
+		}
+	}
+	
+	private class TimeServerHandler extends ChannelHandlerAdapter{
+		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+			
+		}
+		
+		public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+			ctx.flush();
+		}
+		
+		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+			ctx.flush();
+		}
+	}
+	
+}
