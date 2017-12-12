4f8e93f368c167f70a6ab4ffda848ac63c7e39de
leixiangjian <leixiangjian@bolemy.com>
2017-12-12 15:21:28 +0800

    commit;

--- netty/src/main/java/com/netty/study/client/TimeClient.java
+++ netty/src/main/java/com/netty/study/client/TimeClient.java
@@ -1,4 +1,4 @@
-package com.lxj.netty.study.client;
+package com.netty.study.client;
 
 import io.netty.bootstrap.Bootstrap;
 import io.netty.channel.ChannelFuture;
--- netty/src/main/java/com/netty/study/server/TimeServer.java
+++ netty/src/main/java/com/netty/study/server/TimeServer.java
@@ -1,4 +1,4 @@
-package com.lxj.netty.study.server;
+package com.netty.study.server;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
