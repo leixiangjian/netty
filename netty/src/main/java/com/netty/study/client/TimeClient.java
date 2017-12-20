026c48b53c523c340f3eafdee8b783df6a7fc118
leixiangjian <leixiangjian@bolemy.com>
2017-12-20 18:21:16 +0800

    commit;

--- netty/src/main/java/com/netty/study/client/TimeClient.java
+++ netty/src/main/java/com/netty/study/client/TimeClient.java
@@ -1,18 +1,28 @@
 package com.netty.study.client;
 
+import java.util.logging.Logger;
+
 import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class TimeClient {
-	public static void main(String[] args) {
-
+	private static final Logger logger = Logger.getLogger(TimeClient.class.getName());
+	
+	public static void main(String[] args) throws Exception {
+		TimeClient timeClient = new TimeClient();
+		timeClient.connect(9001, "localhost");
 	}
 	
 	public void connect(int port,String host) throws Exception{
@@ -25,6 +35,8 @@ public class TimeClient {
 			.handler(new ChannelInitializer<SocketChannel>() {
 				@Override
 				protected void initChannel(SocketChannel ch) throws Exception {
+					ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+					ch.pipeline().addLast(new StringDecoder());
 					ch.pipeline().addLast(new TimeClientHandler());
 				}
 				
@@ -37,7 +49,34 @@ public class TimeClient {
 		}
 	}
 	
-	private class TimeClientHandler extends ChannelHandlerAdapter{
+	public class TimeClientHandler extends ChannelHandlerAdapter{
+		private int counter;
+		private byte[] req;
+		public TimeClientHandler(){
+			req = ("QUERY TIME ORDER" + System.getProperty("line.separator")).getBytes();
+		}
 		
+		public void channelActive(ChannelHandlerContext ctx){
+			ByteBuf message = null;
+			
+			for(int i=0;i<100;i++){
+				message = Unpooled.buffer(req.length);
+				message.writeBytes(req);
+				ctx.writeAndFlush(message);
+			}
+		}
+		
+		public void ChannelRead(ChannelHandlerContext ctx,Object msg) throws Exception{
+			ByteBuf buf = (ByteBuf)msg;
+			byte[] req = new byte[buf.readableBytes()];
+			buf.readBytes(req);
+			String body = new String(req,"UTF-8");
+			System.out.println("Now is :"+body+";the counter is :"+ ++counter);
+		}
+		
+		public void exceptionCaught(ChannelHandlerContext ctx,Throwable cause){
+			logger.warning("Unexpected exception from downstream:"+cause.getMessage());
+			ctx.close();
+		}
 	}
 }
--- netty/src/main/java/com/netty/study/server/TimeServer.java
+++ netty/src/main/java/com/netty/study/server/TimeServer.java
@@ -1,6 +1,8 @@
 package com.netty.study.server;
 
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
 import io.netty.channel.ChannelFuture;
 import io.netty.channel.ChannelHandlerAdapter;
 import io.netty.channel.ChannelHandlerContext;
@@ -10,56 +12,68 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.codec.LineBasedFrameDecoder;
+import io.netty.handler.codec.string.StringDecoder;
 
 public class TimeServer {
 	public static void main(String[] args) throws Exception {
 		TimeServer timeServer = new TimeServer();
 		timeServer.bind(9001);
 	}
-	
-	private void bind(int port) throws Exception{
-		//两个线程组用户网络事件处理
+
+	private void bind(int port) throws Exception {
+		// 两个线程组用户网络事件处理
 		EventLoopGroup bossGroup = new NioEventLoopGroup();
 		EventLoopGroup workerGroup = new NioEventLoopGroup();
-		
-		try{
+
+		try {
 			ServerBootstrap serverBootstrap = new ServerBootstrap();
-			serverBootstrap.group(bossGroup, workerGroup)
-			.channel(NioServerSocketChannel.class)
-			.option(ChannelOption.SO_BACKLOG, 1024)
-			.childHandler(new ChannelInitializer<SocketChannel>(){
-				@Override
-				protected void initChannel(SocketChannel ch) throws Exception {
-					ch.pipeline().addLast(new TimeServerHandler());					
-				}
-			});
-			
-			//绑定端口，同步等待
+			serverBootstrap.group(bossGroup, workerGroup).channel(NioServerSocketChannel.class)
+					.option(ChannelOption.SO_BACKLOG, 1024).childHandler(new ChannelInitializer<SocketChannel>() {
+						@Override
+						protected void initChannel(SocketChannel ch) throws Exception {
+							ch.pipeline().addLast(new LineBasedFrameDecoder(1024));
+							ch.pipeline().addLast(new StringDecoder());
+							ch.pipeline().addLast(new TimeServerHandler());
+						}
+					});
+
+			// 绑定端口，同步等待
 			ChannelFuture f = serverBootstrap.bind(port).sync();
-			
-			//登陆服务器监听端口关闭
+
+			// 登陆服务器监听端口关闭
 			f.channel().closeFuture().sync();
-			
-			
-		}finally{
-			//优雅退出，释放资源
+
+		} finally {
+			// 优雅退出，释放资源
 			bossGroup.shutdownGracefully();
 			workerGroup.shutdownGracefully();
 		}
 	}
-	
-	private class TimeServerHandler extends ChannelHandlerAdapter{
+
+	private class TimeServerHandler extends ChannelHandlerAdapter {
+		private int counter;
+
 		public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+			String body = (String) msg;
+			System.out.println("The time server receive order : " + body + ";the counter is :" + ++counter);
+
+			String currentTime = "QUERY TIME ORDER".equalsIgnoreCase(body)
+					? new java.util.Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+					
+			currentTime = currentTime + System.getProperty("line.separator");
+			ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
 			
+			ctx.writeAndFlush(resp);
 		}
-		
+
 		public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
 			ctx.flush();
 		}
-		
+
 		public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
 			ctx.flush();
 		}
 	}
-	
+
 }
